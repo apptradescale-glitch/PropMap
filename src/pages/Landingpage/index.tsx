37b2a9a2089c8aa0bba58bc0120dec4bc9b9abbd
Kevin Krall <krall.kevin@web.de>
2026-03-05 06:49:55 -0500

    u

--- src/pages/Landingpage/index.tsx
+++ src/pages/Landingpage/index.tsx
@@ -761,15 +761,15 @@ export default function LandingPage() {
                 fontSize: 22.5,
                 fontWeight: 700,
                 letterSpacing: '-0.04em',
-                color: '#e0ac69',
-                display: 'flex',
+                color: '#ededed',
+                display: 'inline-flex',
                 alignItems: 'center',
               }}>
-                PROP<span style={{ fontSize: 13.75, fontWeight: 400, verticalAlign: 'sub', marginLeft: 1, opacity: 0.7, position: 'relative', top: 2 }}>MAP</span>
+                PROP<span style={{ fontSize: 11, fontWeight: 400, verticalAlign: 'sub', marginLeft: 1, opacity: 0.7, position: 'relative', top: 2 }}>MAP</span>
               </span>
               <span style={{ 
                 fontSize: 22.5, 
-                color: '#e0ac69', 
+                color: '#888', 
                 marginLeft: 8,
                 fontWeight: 700,
                 fontStyle: 'italic',
